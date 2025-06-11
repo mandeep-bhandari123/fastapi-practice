c9bb5c600b41ecb1251ccbad5afd64e3f97a2704
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-06-11 23:10:16 +0545

    Code Update

--- app/models.py
+++ app/models.py
@@ -25,5 +25,5 @@ class User(Base):
     
 class Votes(Base):
     __tablename__ = "votes"
-    user_id=Column(Integer, ForeignKey("users.id",ondelete="CASCADE"),primary_key=True)
-    post_id=Column(Integer, ForeignKey("posts.id",ondelete="CASCADE"),primary_key= False)
+    user_id=Column(Integer, ForeignKey("users.id",ondelete="CASCADE"),primary_key = True)
+    post_id=Column(Integer, ForeignKey("posts.id",ondelete="CASCADE"),primary_key = True)
