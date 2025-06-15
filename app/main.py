17d66ae01ea1bcb6ee0be48a0be0bb08fc9555ca
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-06-15 22:56:11 +0545

    Code Update

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 # from sqlalchemy.orm import session
-from .routers import post , user , auth
+from .routers import post , user , auth , vote
 
 
 app = FastAPI()
@@ -34,6 +34,7 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 def root():  
--- app/routers/vote.py
+++ app/routers/vote.py
@@ -10,6 +10,9 @@ router = APIRouter(
 def vote(vote:schemas.Vote, db:Session=Depends(database.get_db), current_user:int = Depends(oauth2.get_current_user)):
     vote_query=db.query(models.Votes).filter(models.Votes.post_id == vote.post_id , models.Votes.user_id == current_user.id)
     
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
     found_vote = vote_query.first()
     if (vote.dir == 1):
         if found_vote:
@@ -22,5 +25,8 @@ def vote(vote:schemas.Vote, db:Session=Depends(database.get_db), current_user:in
         if not found_vote:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Vote does not exist")
         
-        vote_query.delete()
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+        
+        return{"message":"Sucessfully Deleted Vote"}
         
